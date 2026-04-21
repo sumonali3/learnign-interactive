93097e12e06df9aa041dc435f3b3d2937b16d4e1
Md Sumon Ali Akondo <sumonsum33@gmail.com>
2026-04-22 00:55:18 +0600

    js 1st class practive

--- inc/custom.js
+++ inc/custom.js
@@ -1,2 +1,18 @@
 // this is first comment 
 console.log("Hello world");
+
+//js variables  
+var firstName = "sumon";
+let lastName = "ali";
+const age = 25;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+var firstName = "sumon ali";
+lastName = "khan";
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
